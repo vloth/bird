86ebb7f726f4980b46feec76f2a56eff4bcfadc6
vloth <fran.fhb@gmail.com>
2020-07-25 17:28:44 +0200

    Σ feat: Initial commit, transpose bird from embrio

---
+++ README.md
@@ -0,0 +1,8 @@
+## São Paulo 2020-07-25 18:00:00
+![sp](Sao_Paulo_2020-07-25_18_00_00.png)
+
+## Amsterdam 2020-07-25 18:00:00
+![amsterdam](Amsterdam_2020-07-25_18_00_00.png)
+
+## Colombo 2020-07-25 18:00:00
+![colombo](Colombo_2020-07-25_18_00_00.png)
---
+++ bird.R
@@ -0,0 +1,47 @@
+suppressPackageStartupMessages(library(dplyr))
+
+plot <- function(trajectory, name) {
+  canvas = ggplot2::theme(legend.position  = "none",
+    panel.background = ggplot2::element_rect(fill="white"),
+    axis.ticks = ggplot2::element_blank(),
+    panel.grid = ggplot2::element_blank(),
+    axis.title = ggplot2::element_blank(),
+    axis.text = ggplot2::element_blank())
+
+  str_path <- gsub(" ", "_", paste(iconv(name, from = 'UTF-8', to = 'ASCII//TRANSLIT'), ".png", sep=""))
+  str_path <- gsub(":", "_", gsub("~", "", str_path))
+  png(str_path, units="px", width=1600, height=1600, res=200)
+  ggplot2::ggplot(trajectory, ggplot2::aes(x, y)) +
+      ggplot2::geom_point(color="black", shape=46, alpha=.01) +
+   canvas
+}
+
+Rcpp::cppFunction('
+  DataFrame gen_path(int n, double x0, double y0, 
+    double a, double b, double c, double d) {
+    NumericVector x(n); NumericVector y(n);
+    x[0]=x0; y[0]=y0;
+    for(int i = 1; i < n; ++i) {
+      x[i] = sin(a*y[i-1])+c*cos(a*x[i-1]);
+      y[i] = sin(b*x[i-1])+d*cos(b*y[i-1]);
+    }
+    return DataFrame::create(_["x"]= x, _["y"]= y);
+  }
+')
+
+# name = "São Paulo"
+name = "Colombo"
+forecast_sp <- owmr::get_forecast(name) %>% owmr::owmr_as_tibble()
+
+a = ((forecast_sp $wind_speed %>% median) * -1) + 2.379
+b = ((forecast_sp $wind_speed %>% max) - (forecast_sp $wind_speed %>% min)) - 2.379
+c = forecast_sp $wind_speed %>% min
+d = ((forecast_sp $wind_speed %>% median) * 1.2938) - 2.2389028
+
+str(a)
+str(b)
+str(c)
+str(d)
+
+p <- gen_path(10000000, 0, 0, a, b, c, d)
+plot(p, paste(name, forecast_sp $dt_txt[1]))
