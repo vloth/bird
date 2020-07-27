c9924fd1d4a18aa8a4a7faa85b7311b527f5c756
vloth <fran.fhb@gmail.com>
2020-07-27 20:04:44 +0200

    Γ build: Add build folder

--- binoculars/build.js
+++ binoculars/build.js
@@ -11,32 +11,42 @@ const mkdir = promisify(fs.mkdir)
 
 async function ensureDirExists(dir) {
   if (await exists(dir)) return
-  return mkdir(dir)
+  return mkdir(dir, { recursive: true })
 }
 
 async function writeHtmlOut(baseHtml, birds) {
-    const birdsHtml = birds.map(bird => ```
-      <div class='bird'>
-        <img src="assets/thumb/${bird}" />
-        <span class='description'>${bird}</span>
-      </div>
-    ```)
+  const birdsHtml = birds.map(bird => `
+    <div class='bird'>
+      <img src="assets/thumb/${bird}" alt="" />
+      <span class='description'>Bla bla</span>
+    </div>
+   `).join('')
    const outHtml = baseHtml.replace('<birds />', birdsHtml);
    await writeFile('build/index.html', outHtml)
 }
 
-async function main() {
-  const files = await readdir('../out/')
-
-  const copies = files.map(x => copy(`../out/${x}`, `assets/out/${x}`))
-  const thumbs = files.map(x => sharp(`../out/${x}`) .resize(480)
+async function copyFiles(files) {
+  const copies = files.map(x => copy(`../out/${x}`, `build/assets/out/${x}`))
+  const thumbs = files.map(x => sharp(`../out/${x}`).resize(480)
      .png({ progressive: true, quality: 45 })
-     .toFile(`assets/thumb/${x}`))
+     .toFile(`build/assets/thumb/${x}`))
 
-  const files = Promise.all(copies.concat(thumbs))
+  return Promise.all(copies.concat(thumbs))
+}
 
-  await ensureDirExists('build')
-  await writeHtmlOut(await readFile('src/index.html'), files)
+async function main() {
+  await Promise.all([
+    ensureDirExists('build/css'),
+    ensureDirExists('build/assets/out'),
+    ensureDirExists('build/assets/thumb'),
+  ])
+
+  const files = await readdir('../out')
+
+  await Promise.all([copyFiles(files), copy('css/base.css', 'build/css/base.css')])
+
+  const template = await readFile('index.html', { encoding: 'utf8' })
+  await writeHtmlOut(template, files)
 }
 
 main()
